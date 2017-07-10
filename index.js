cba98e6f96d9e6a0fc023c7612d40a533b29e43d
Miguel Vicente Linares <miviclin@gmail.com>
2017-07-10 11:28:44 +0200

    Initial commit. Add swagger-generated project and implementation

---
+++ index.js
@@ -0,0 +1,40 @@
+'use strict';
+
+let app = require('connect')();
+let http = require('http');
+let swaggerTools = require('swagger-tools');
+let jsyaml = require('js-yaml');
+let fs = require('fs');
+let serverPort = 8000;
+
+// swaggerRouter configuration
+let options = {
+  swaggerUi: '/swagger.json',
+  controllers: './lib/controller',
+  useStubs: process.env.NODE_ENV === 'development' ? true : false // Conditionally turn on stubs (mock mode)
+};
+
+// The Swagger document (require it, build it programmatically, fetch it from a URL, ...)
+let spec = fs.readFileSync('./api/swagger.yaml', 'utf8');
+let swaggerDoc = jsyaml.safeLoad(spec);
+
+// Initialize the Swagger middleware
+swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
+  // Interpret Swagger resources and attach metadata to request - must be first in swagger-tools middleware chain
+  app.use(middleware.swaggerMetadata());
+
+  // Validate Swagger requests
+  app.use(middleware.swaggerValidator());
+
+  // Route validated requests to appropriate controller
+  app.use(middleware.swaggerRouter(options));
+
+  // Serve the Swagger documents and Swagger UI
+  app.use(middleware.swaggerUi());
+
+  // Start the server
+  http.createServer(app).listen(serverPort, function () {
+    console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
+    console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
+  });
+});
